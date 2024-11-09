48d0876a43bb582d22ad10e3de7f71716190d87f
Yuvallevi60 <137276987+Yuvallevi60@users.noreply.github.com>
2024-11-09 21:50:00 +0200

    setting the base files

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post ("/add", (req, res) => {
+    const headLine = req.body["headLine"];
+    res.render("index.ejs", { headLine });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
