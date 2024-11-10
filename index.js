e46f83a6dc1c1ce5a5ce903baf6e98663033466d
Yuvallevi60 <137276987+Yuvallevi60@users.noreply.github.com>
2024-11-11 00:42:24 +0200

    completing the basic
    -connecting to Google books' API to get a url code for a given book's cover
    
    -when user press the "Add" button a "review" will be saved on a postgresSQL databas. 'review' object includes: the book's name, review rate and the book's cover.
    
    -displaying a list of all the book reviews  that was saved to the data base.

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import axios from "axios";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "BookNotes",
+    password: "postgres",
+    port: 5433,
+});
+db.connect();
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    const result = await db.query("SELECT * FROM reviews");
+    const reviews = result.rows;
+    res.render("index.ejs", { reviews });
 });
 
-app.post ("/add", (req, res) => {
-    const headLine = req.body["headLine"];
-    res.render("index.ejs", { headLine });
+
+app.post ("/add", async (req, res) => {
+    const bookTitle = req.body.bookTitle;
+    const bookRate = req.body.bookRate;
+    console.log(bookTitle);
+
+    let coverUrl = null;
+    try {
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`);
+        const bookData = response.data.items[0];
+
+        if (bookData && bookData.volumeInfo.imageLinks) {
+            coverUrl = bookData.volumeInfo.imageLinks.thumbnail;
+        }
+        await db.query("INSERT INTO reviews (book_title, rate, cover_url) VALUES ($1, $2, $3)", [bookTitle, bookRate, coverUrl])
+
+        res.redirect("/");
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 });
 
 app.listen(port, () => {
