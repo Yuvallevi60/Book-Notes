781d57281fe1320f362163335883f766a686dc46
Yuvallevi60 <137276987+Yuvallevi60@users.noreply.github.com>
2024-11-11 23:20:37 +0200

    little styling + book's title autocorrect
    -add stlye to the reviews list.
    
    -the title of the book that saved to the DB is the one the return from the Google book API and not what the user insert.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const db = new pg.Client({
 });
 db.connect();
 
+app.use(express.static("public")); 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", async (req, res) => {
@@ -25,7 +26,7 @@ app.get("/", async (req, res) => {
 
 
 app.post ("/add", async (req, res) => {
-    const bookTitle = req.body.bookTitle;
+    let bookTitle = req.body.bookTitle;
     const bookRate = req.body.bookRate;
     console.log(bookTitle);
 
@@ -34,8 +35,11 @@ app.post ("/add", async (req, res) => {
         const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`);
         const bookData = response.data.items[0];
 
+        console.log(bookData.volumeInfo.title)
+
         if (bookData && bookData.volumeInfo.imageLinks) {
             coverUrl = bookData.volumeInfo.imageLinks.thumbnail;
+            bookTitle = bookData.volumeInfo.title;
         }
         await db.query("INSERT INTO reviews (book_title, rate, cover_url) VALUES ($1, $2, $3)", [bookTitle, bookRate, coverUrl])
 
